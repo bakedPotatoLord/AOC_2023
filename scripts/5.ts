4a5aae443fad13300c3a4d67161a998f06b6dbab
bakedpotatolord <jeffy9745@gmail.com>
2023-12-10 14:39:24 -0700

    solve day5 part one and start part 2

--- scripts/5.ts
+++ scripts/5.ts
@@ -1,36 +1,53 @@
-import fs from 'node:fs/promises'
 import {getInput, numberSum} from './helpers'
 
 const raw = (await getInput(5))
 
 const seeds = raw.split('\n')[0].split(": ")[1].trim().split(' ').map(el=>parseInt(el))
 
-//console.log(seeds)
 
 const maps = raw.split('\n\n').slice(1).map(map=>
   map.split(':')[1].trim().split('\n').map(submap=>
     (<[number,number,number]>submap.split(' ').map(val=>parseInt(val)))
   )
 )
-.map((key,idx)=>{
-  const map = new Map<number,number>()
-  for(let [dest,source,len] of key){
-    console.log(len)
-    const diff = dest - source
-    for(let i = source;i<source+len;i++){
-      map.set(i,i+diff)
-      console.log((i-source)/len)
+.map((key)=>{
+  return (num:number)=>{
+    for(let [dest,source,len] of key){
+      const diff = dest - source
+      if(num >= source && num < source+len){
+        return num + diff
+      }
     }
+    return num
   }
-  console.log(idx)
-  return map
 })
 
 
-function getOutputs(keys:number[],map:Map<number,number>){
-  return keys.map(key=> map.has(key) ? map.get(key) : key)
+function getOutputs(keys:number[],map:typeof maps[0] ){
+  return keys.map(k=> map(k))
+}
+let locations = maps.reduce((prev,curr)=>getOutputs(prev,curr),seeds)
+console.log("part 1:", Math.min(...locations))
+
+
+
+
+function* getSeeds(){
+  for(let i = 0;i<seeds.length;i+=2){
+    
+    for(let j = 0;j<seeds[i+1];j++){
+      console.log(j/seeds[i+1])
+      yield seeds[i]+j
+    }
+  }
 }
 
-let locations = maps.reduce((prev,curr)=>getOutputs(prev,curr),seeds)
+const realSeeds = Array.from(getSeeds())
 
-console.log("part 1:", Math.min(...locations))
+
+const locations2 = maps.reduce((prev,curr)=>{
+  console.log(getOutputs(prev,curr))
+  return getOutputs(prev,curr)
+},realSeeds.flat())
+
+console.log("part 2:", Math.min(...locations2))
