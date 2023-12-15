77579cec94ab249736a486c52998a9a068ffbce9
bakedpotatolord <jeffy9745@gmail.com>
2023-12-14 19:16:10 -0700

    extremely hacky solution for part 1

--- scripts/7.ts
+++ scripts/7.ts
@@ -1,12 +1,9 @@
-import { getCommentRange } from '../node_modules/typescript/lib/typescript'
 import {getInput, numberSum} from './helpers.js'
 
 const raw: [string,number][] = (await getInput(7))
 .split("\n")
 .map(l=>l.split(" "))
 .map(l=>[l[0],parseInt(l[1])])
-
-
 
 const fiveOfAKind = /(.)\1{4}/
 const fourOfAKind = /(.)\1{3}/
@@ -15,14 +12,25 @@ const threeOfAKind = /(.)\1{2}/
 const twoPair = /(.)\1{1}(.)\2{1}|(.)\3{1}.(.)\4{1}/
 const pair =  /(.)\1/
 
-console.log(raw)
-console.log(raw.map(h=>getRank(h[0])))
 
 const cardState = {
   A:"A",
   K:"B",
-  
+  Q:"C",
+  J:"D",
+  "T":"E",
+  "9":"F",
+  "8":"G",
+  "7":"H",
+  "6":"I",
+  "5":"J",
+  "4":"K",
+  "3":"L",
+  "2":"M",
+
 }
+
+const replaced:[string,number,string][] = raw.map(l=>[...l,Array.from(l[0],c=>cardState[c]).join("")])
 
 function getRank(hand:string){
   const sorted = hand.split("").sort().join("");
@@ -37,7 +45,33 @@ function getRank(hand:string){
 
 }
 
-console.log(
+let cards: ( typeof replaced)[] = Array(11).fill(undefined).map(_=>[])
+
+for(let hand of replaced){
+  const rank = getRank(hand[0])
+  cards[rank].push(hand)
+}
+
+cards.forEach(rank=>{
+  rank.sort((a,b)=>{
+    const comp =
+    -a[2].localeCompare(b[2]);
+    if(comp === 0){
+      throw Error("wtf")
+    }
+    return comp
   
-)
+  })
+})
+
+const flat = cards.flat()
+
+let winnings = 0 
+
+flat.forEach(([_,val],idx)=>{
+  winnings += (val * (idx+1))
+})
+
+
+console.log("part 1",winnings) 
 
