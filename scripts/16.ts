018f6ccc5dad51bfcb0901e2281e0920a9bcfce7
bakedpotatolord <jeffy9745@gmail.com>
2023-12-23 11:57:45 -0700

    solve part 2

--- scripts/16.ts
+++ scripts/16.ts
@@ -3,6 +3,8 @@ import {getInput, numberSum} from './helpers'
 const matrix = (await getInput(16))
 .split("\n").map(row=>row.split("").map(char=>new MazeNode(char)))
 
+const flatMatrix = matrix.flat()
+
 enum Direction{
   left,
   right,
@@ -22,9 +24,10 @@ class Lazer{
     this.dir = dir
   }
 
-  move(){
+  move(lazerCollector:Lazer[]){
     const node =matrix[this.y][this.x]
     node.energized = true
+    node.dirSet.add(this.dir)
     const char = node.char
     if(char == "\\"){
       if(this.dir == Direction.down) this.dir = Direction.right
@@ -37,21 +40,32 @@ class Lazer{
       else if(this.dir == Direction.left) this.dir = Direction.down
       else if(this.dir == Direction.right) this.dir = Direction.up
     }else if(char == "|"){
-      if(this.dir == Direction.left){
+      if(this.dir == Direction.left || this.dir == Direction.right){
         this.dir = Direction.up
+        lazerCollector.push(new Lazer(this.x,this.y,Direction.down))
       }
+    }else if(char ==  "-"){
+      if(this.dir == Direction.up || this.dir == Direction.down){
+        this.dir = Direction.left
+        lazerCollector.push(new Lazer(this.x,this.y,Direction.right))
+      }
+
     }
 
-    if(this.dir == Direction.down) this.y--
-    else if(this.dir == Direction.up) this.y++
+    if(this.dir == Direction.down) this.y++
+    else if(this.dir == Direction.up) this.y--
     else if(this.dir == Direction.left) this.x--
     else if(this.dir == Direction.right) this.x++
     
-    
   }
 
-  outsideMatrix(x,y){
+  outsideMatrix(){
+    return this.x< 0 || this.y< 0 || this.x > matrix[0].length -1 || this.y > matrix.length -1
+  }
 
+  isClone(){
+    const node =matrix[this.y][this.x]
+    return node.dirSet.has(this.dir)
   }
 }
 
@@ -63,15 +77,54 @@ class MazeNode{
   constructor(char:string){
     this.char = char
   }
-}
 
-const lazers = [new Lazer(0,0,Direction.right)]
 
-while(lazers.length){
-  const next = lazers.pop()
-
-  while(true){
-
+  reset(){
+    this.energized = false
+    this.dirSet.clear()
   }
-
 }
+part1:{
+
+  const lazers = [new Lazer(0,0,Direction.right)]
+  
+  while(lazers.length){
+    const next = lazers.pop()
+  
+    while(true){
+      next.move(lazers)
+      if(next.outsideMatrix() || next.isClone()) break
+      //console.log(next.x,next.y)
+    }
+  }
+  
+  console.log("part 1: ",flatMatrix.reduce((a,b)=>a+ (b.energized? 1:0),0))
+}
+
+const lazerPossibility:Lazer[] = [
+  ...Array(matrix.length).fill(undefined).map((_,i)=>new Lazer(0,i,Direction.right)),
+  ...Array(matrix.length).fill(undefined).map((_,i)=>new Lazer(matrix.length-1,i,Direction.left)),
+  ...Array(matrix[0].length).fill(undefined).map((_,i)=>new Lazer(i,0,Direction.down)),
+  ...Array(matrix[0].length).fill(undefined).map((_,i)=>new Lazer(i,matrix[0].length-1,Direction.down)),
+]
+
+let maxEnergized = 0
+
+while(lazerPossibility.length){
+  const next = lazerPossibility.pop()
+  //reset matrix
+  flatMatrix.forEach(node=>node.reset())
+  //create inner lazer queue
+  const lazers = [next]
+  while(lazers.length){
+    const next = lazers.pop()
+    while(true){
+      next.move(lazers)
+      if(next.outsideMatrix() || next.isClone()) break
+    }
+  }
+  const numEnergized = flatMatrix.reduce((a,b)=>a+ (b.energized? 1:0),0)
+  maxEnergized = Math.max(maxEnergized,numEnergized)
+}
+
+console.log("part 2: ",maxEnergized)
